f03db755dcdee39c27ba85ade22d71e65c9d3bc5
Junior J. Victoriano <juniorveloz94@gmail.com>
2026-02-12 21:36:37 -0400

    Inclusión de archivo TXT para el testeo del ejercicio 15
    Dentro del archivo se alojaran los textos de prueba.

--- Ejercicio15/Form1.Designer.cs
+++ Ejercicio15/Form1.Designer.cs
@@ -28,18 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            openFileDialog1 = new OpenFileDialog();
+            saveFileDialog1 = new SaveFileDialog();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(366, 124);
+            textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 151);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Open file";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(93, 151);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Save file";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(467, 242);
+            ClientSize = new Size(394, 186);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private OpenFileDialog openFileDialog1;
+        private SaveFileDialog saveFileDialog1;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
     }
 }
--- Ejercicio15/Form1.cs
+++ Ejercicio15/Form1.cs
@@ -6,5 +6,21 @@ namespace Ejercicio15
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, textBox1.Text);
+            }
+        }
     }
 }
