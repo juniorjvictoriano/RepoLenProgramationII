1be3e3690fc4fe41c4338cf5dc6b84fbc7bdea1f
Junior J. Victoriano <juniorveloz94@gmail.com>
2026-02-12 20:10:02 -0400

    Ejercicio 7
    Diseña un formulario con una lista (ListBox) y un botón. Permite agregar elementos ingresados en una caja de texto a la lista.

---
+++ Ejercicio7/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Ejercicio7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(38, 29);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(127, 19);
+            listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(171, 120);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(38, 120);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Ingresa un texto";
+            textBox1.Size = new Size(127, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(411, 180);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private Button button1;
+        private TextBox textBox1;
+    }
+}
