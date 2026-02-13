068ba50236570c104568ddb5145dfd79113012d7
Junior J. Victoriano <juniorveloz94@gmail.com>
2026-02-12 20:25:43 -0400

    Ejercicio 8 funcionando

--- Ejercicio8/Form1.Designer.cs
+++ Ejercicio8/Form1.Designer.cs
@@ -28,18 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 54);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(467, 241);
+            ClientSize = new Size(184, 108);
+            Controls.Add(button1);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button button1;
     }
 }
--- Ejercicio8/Form1.cs
+++ Ejercicio8/Form1.cs
@@ -2,9 +2,17 @@ namespace Ejercicio8
 {
     public partial class Form1 : Form
     {
+        private int contador = 0;
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            contador++;
+            label1.Text = $"Conteo: {contador}";
         }
     }
 }
