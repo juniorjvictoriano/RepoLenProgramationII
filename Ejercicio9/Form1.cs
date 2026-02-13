b3fc62a36c7c0ffa877567b37e6f95f09e3b32d4
Junior J. Victoriano <juniorveloz94@gmail.com>
2026-02-12 20:38:25 -0400

    Ejercicio 9 funcionando

--- Ejercicio9/Form1.cs
+++ Ejercicio9/Form1.cs
@@ -6,5 +6,27 @@ namespace Ejercicio9
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out _))
+            {
+                MessageBox.Show(
+                    "El valor ingresado es un número válido",
+                    "Correcto",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Error: debe ingresar un número",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
+        }
     }
 }
